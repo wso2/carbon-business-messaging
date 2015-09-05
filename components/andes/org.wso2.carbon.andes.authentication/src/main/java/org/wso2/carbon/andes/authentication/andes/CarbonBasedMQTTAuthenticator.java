1c78372cfdd4a8e62ddd21436c9b0ef5644c434e
Hemika Yasinda Kodikara <hemika.kodikara@gmail.com>
2015-09-05 17:46:21 +0530

    Adding tenant flow for mqtt auths

--- components/andes/org.wso2.carbon.andes.authentication/src/main/java/org/wso2/carbon/andes/authentication/andes/CarbonBasedMQTTAuthenticator.java
+++ components/andes/org.wso2.carbon.andes.authentication/src/main/java/org/wso2/carbon/andes/authentication/andes/CarbonBasedMQTTAuthenticator.java
@@ -21,6 +21,7 @@ package org.wso2.carbon.andes.authentication.andes;
 import org.apache.log4j.Logger;
 import org.dna.mqtt.moquette.server.IAuthenticator;
 import org.wso2.carbon.andes.authentication.internal.AuthenticationServiceDataHolder;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
 import org.wso2.carbon.user.api.TenantManager;
 import org.wso2.carbon.user.api.UserRealm;
 import org.wso2.carbon.user.api.UserStoreException;
@@ -36,6 +37,7 @@ import org.wso2.carbon.utils.multitenancy.MultitenantUtils;
 public class CarbonBasedMQTTAuthenticator implements IAuthenticator {
 
     private static final Logger logger = Logger.getLogger(CarbonBasedMQTTAuthenticator.class);
+    private static final String DOMAIN_NAME_SEPARATOR = "!";
 
     /**
      * {@inheritDoc} Authenticates the user against carbon user store.
@@ -44,38 +46,51 @@ public class CarbonBasedMQTTAuthenticator implements IAuthenticator {
     public boolean checkValid(String username, String password) {
 
         boolean isAuthenticated = false;
-
         // Carbon kernel uses '@' to separate domain while MB uses '!'
-        String carbonCompliantUsername = username.replace('!', '@');
+        String carbonCompliantUsername = username.replace(DOMAIN_NAME_SEPARATOR, "@");
 
         try {
+            PrivilegedCarbonContext.startTenantFlow();
+
             int tenantId = getTenantIdOfUser(carbonCompliantUsername);
 
             if (MultitenantConstants.INVALID_TENANT_ID != tenantId) {
-                UserRealm userRealm =
-                                      AuthenticationServiceDataHolder.getInstance().getRealmService()
-                                                                     .getTenantUserRealm(tenantId);
+                if (username.contains(DOMAIN_NAME_SEPARATOR)) {
+                    String tenantDomain = username.substring(username.indexOf(DOMAIN_NAME_SEPARATOR) + 1);
+                    PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantDomain(tenantDomain);
+                    PrivilegedCarbonContext.getThreadLocalCarbonContext().getTenantId(true);
+                } else {
+                    PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantId(MultitenantConstants
+                                                                                                    .SUPER_TENANT_ID);
+                    PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantDomain(MultitenantConstants
+                                                                                            .SUPER_TENANT_DOMAIN_NAME);
+                }
+
+                UserRealm userRealm = AuthenticationServiceDataHolder.getInstance().getRealmService()
+                                                                                        .getTenantUserRealm(tenantId);
                 UserStoreManager userStoreManager = userRealm.getUserStoreManager();
                 isAuthenticated = userStoreManager.authenticate(
-                                          MultitenantUtils.getTenantAwareUsername(carbonCompliantUsername), password);
+                                         MultitenantUtils.getTenantAwareUsername(carbonCompliantUsername), password);
             } else {
-                logger.error(String.format("access denied, unable to find a tenant for user name : %s", username));
+                logger.error(String.format("Access denied, unable to find a tenant for user name : %s", username));
             }
 
         } catch (UserStoreException e) {
-            String errorMsg = String.format("unable to authenticate user : %s", username);
+            String errorMsg = String.format("Unable to authenticate user : %s", username);
             logger.error(errorMsg, e);
+        } finally {
+            PrivilegedCarbonContext.endTenantFlow();
         }
 
         return isAuthenticated;
     }
 
     /**
-     * Returns tenent Id given the user name or returns
+     * Returns tenant Id given the user name or returns
      * {@link MultitenantConstants#INVALID_TENANT_ID} if none can be found.
      * 
-     * @param username
-     * @return
+     * @param username The username of the user.
+     * @return The tenant ID.
      * @throws UserStoreException
      */
     private int getTenantIdOfUser(String username) throws UserStoreException {
