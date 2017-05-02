67a357da4d2321c1ce66ace5987e095818b931ae
hastef88 <hastef.ds@gmail.com>
2017-05-02 13:59:16 +0530

    Initial sample for C5 component+feature

---
+++ components/org.wso2.carbon.business.messaging.core/src/main/java/org/wso2/carbon/business/messaging/core/Greeter.java
@@ -0,0 +1,13 @@
+package org.wso2.carbon.business.messaging.core;
+
+/**
+ * This interface contains methods for greeting.
+ *
+ * @since 4.0.0-SNAPSHOT
+ */
+public interface Greeter {
+
+    void hello();
+
+    void bye();
+}
---
+++ product/carbon-home/README.md
@@ -0,0 +1,1 @@
+This directory will contain any and all configurations, database scripts positioned into the product distribution. 
