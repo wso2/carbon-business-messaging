9711c445259a17f36499734b3648ba2a1b8d9ba1
Milan Perera <milan@wso2.com>
2017-09-07 13:05:38 +0530

    Refactored the AndesBrokerManager service component

--- components/andes/org.wso2.carbon.andes.admin.mqtt/src/main/java/org/wso2/carbon/andes/admin/mqtt/internal/AndesBrokerManagerMQTTAdminServiceDS.java
+++ components/andes/org.wso2.carbon.andes.admin.mqtt/src/main/java/org/wso2/carbon/andes/admin/mqtt/internal/AndesBrokerManagerMQTTAdminServiceDS.java
@@ -1,21 +1,22 @@
 /*
- * Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *   WSO2 Inc. licenses this file to you under the Apache License,
- *   Version 2.0 (the "License"); you may not use this file except
- *   in compliance with the License.
- *   You may obtain a copy of the License at
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *   Unless required by applicable law or agreed to in writing,
- *   software distributed under the License is distributed on an
- *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *   KIND, either express or implied.  See the License for the
- *   specific language governing permissions and limitations
- *   under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-package org.wso2.carbon.andes.admin.mqtt.util;
+
+package org.wso2.carbon.andes.admin.mqtt.internal;
 
 import org.osgi.service.component.ComponentContext;
 import org.wso2.carbon.andes.authentication.service.AuthenticationService;
@@ -23,7 +24,7 @@ import org.wso2.carbon.andes.core.QueueManagerService;
 import org.wso2.carbon.andes.core.SubscriptionManagerService;
 
 /**
- * this class is used to get the QueueMangerInterface service. it is used to send the
+ * This class is used to get the QueueMangerInterface service. it is used to send the
  * requests received from the Admin service to real cep engine
  *
  * @scr.component name="AndesQueueManagerAdminMQTT.component" immediate="true"
@@ -64,15 +65,14 @@ public class AndesBrokerManagerMQTTAdminServiceDS {
     }
 
     protected void setSubscriptionManagerService(SubscriptionManagerService subscriptionManagerService) {
-        AndesBrokerManagerMQTTAdminServiceDSHolder.getInstance().registerSubscriptionManagerService
-                (subscriptionManagerService);
+        AndesBrokerManagerMQTTAdminServiceDSHolder.getInstance().
+                registerSubscriptionManagerService(subscriptionManagerService);
     }
 
     protected void unSetSubscriptionManagerService(SubscriptionManagerService subscriptionManagerService) {
-        AndesBrokerManagerMQTTAdminServiceDSHolder.getInstance().unRegisterSubscriptionManagerService
-                (subscriptionManagerService);
+        AndesBrokerManagerMQTTAdminServiceDSHolder.getInstance().
+                unRegisterSubscriptionManagerService(subscriptionManagerService);
     }
-
 
     protected void setAccessKey(AuthenticationService authenticationService) {
         AndesBrokerManagerMQTTAdminServiceDSHolder.getInstance().setAccessKey(authenticationService.getAccessKey());
